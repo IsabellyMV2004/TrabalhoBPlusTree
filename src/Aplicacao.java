64a2c1db6fd0ff6c25f9ede9e5b8d8adb05c1591
IsabellyMV2004 <isabellymarafaovoltareli2004@gmail.com>
2025-05-18 18:41:23 -0300

    retirando informações que não vou usar

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -1,16 +1,27 @@
 public class Aplicacao
 {
+
+
+    public char menu(){
+        System.out.println("# # # B+ TREE # # #");
+        System.out.println("\n[A] Inserir");
+        System.out.println("[B] Excluir");
+    }
+
+
     public static void main(String[] args) {
         BTree b = new BPlusTree();
 
+        char op = menu();
 
-        for(int i=1; i<=1000; i++)
-            b.inserir(i, i);
-        System.out.println("------");
-
-        //Teste da exclusão
-        for(int i=6; i<=995; i++)
-            b.exclusao(i);
+        do{
+            switch (op){
+                case 'A':
+                    break;
+                case 'B':
+            }
+            op = menu();
+        }while(op!=27);
 
         b.in_ordem();
     }
--- src/No.java
+++ src/No.java
@@ -32,24 +32,12 @@ public class No
 
     public void remanejar(int pos)
     {
-        vLig[tl+1] = vLig[tl];
-        for(int i=tl; i>pos; i--)
-        {
-            vInfo[i] = vInfo[i-1];
-            vPos[i] = vPos[i-1];
-            vLig[i] = vLig[i-1];
-        }
+
     }
 
     public void remanejarExclusao(int pos)
     {
-        for(int i=pos; i<tl-1; i++)
-        {
-            vInfo[i] = vInfo[i+1];
-            vPos[i] = vPos[i+1];
-            vLig[i] = vLig[i+1];
-        }
-        vLig[tl-1] = vLig[tl];
+
     }
 
     public int getvInfo(int p) {
