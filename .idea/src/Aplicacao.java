84f247e646fab6fd357c48f202f4dd0ee11ed02c
IsabellyMV2004 <isabellymarafaovoltareli2004@gmail.com>
2025-05-18 18:18:52 -0300

    começo do trabalho

---
+++ .idea/src/Aplicacao.java
@@ -0,0 +1,17 @@
+public class Aplicacao
+{
+    public static void main(String[] args) {
+        BTree b = new BTree();
+
+
+        for(int i=1; i<=1000; i++)
+            b.inserir(i, i);
+        System.out.println("------");
+
+        //Teste da exclusão
+        for(int i=6; i<=995; i++)
+            b.exclusao(i);
+
+        b.in_ordem();
+    }
+}
